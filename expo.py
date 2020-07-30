d173de87fafc72a83832b044e5de13089488b3c0
Martinjr8 <marcinmarcin8jr@gmail.com>
2020-07-30 15:46:26 +0200

    class to def, solution

--- expo.py
+++ expo.py
@@ -1,3 +1,31 @@
-class Expo:
-    def __init__(self, base, power):
-        pass
+def expo(base, power):
+    if base == 0 and power == 0:
+        raise ArithmeticError
+    if base == 1 or power == 0:
+        return 1
+    if base == 0:
+        return 0
+    if (base != 0 or base != 1) and power > 0:
+        result = base
+        while power > 1:
+            result *= base
+            power -= 1
+        if base < 0 < result:
+            result -= 2 * result
+        return result
+    if (base != 0 or base != 1) and power < 0:
+        result = base
+        power = abs(power)
+        while power > 1:
+            result *= base
+            power -= 1
+        if base < 0 < result:
+            result -= 2 * result
+        return 1 / result
+
+
+
+if __name__ == "__main__":
+    base = int(input())
+    power = int(input())
+    result = expo(base, power)
--- expo_test.py
+++ expo_test.py
@@ -1,35 +1,35 @@
 import unittest
-from expo import Expo
+from expo import expo
 
 
 class ExpoTest(unittest.TestCase):
     def test_both_positive(self):
-        result = Expo(2, 2)
+        result = expo(2, 2)
         self.assertEqual(result, 4)
 
     def test_positive_to_negative(self):
-        result = Expo(2, -2)
+        result = expo(2, -2)
         self.assertEqual(result, 0.25)
 
     def test_negative_to_positive(self):
-        result = Expo(-2, 2)
+        result = expo(-2, 2)
         self.assertEqual(result, -4)
 
     def test_both_negative(self):
-        result = Expo(-2, -2)
+        result = expo(-2, -2)
         self.assertEqual(result, -0.25)
 
     def test_base_to_zero(self):
-        result = Expo(2, 0)
+        result = expo(2, 0)
         self.assertEqual(result, 1)
 
     def test_zero_to_power(self):
-        result = Expo(0, 2)
+        result = expo(0, 2)
         self.assertEqual(result, 0)
 
     def test_both_zero(self):
-        with self.assertRaises(Exception):
-            Expo(0, 0)
+        with self.assertRaises(ArithmeticError):
+            expo(0, 0)
 
 
 if __name__ == "__main__":
